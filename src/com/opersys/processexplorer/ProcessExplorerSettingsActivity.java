c264b807654a38fd4a17d8cb23e6cc30c68a6135
François-Denis Gonthier <francois-denis.gonthier@opersys.com>
2014-04-09 21:46:49 -0400

    UI improvements. Better start/stop handling in the UI.

--- src/com/opersys/processexplorer/ProcessExplorerSettingsActivity.java
+++ src/com/opersys/processexplorer/ProcessExplorerSettingsActivity.java
@@ -40,23 +40,6 @@ public class ProcessExplorerSettingsActivity extends PreferenceActivity
             public boolean onPreferenceClick(Preference preference) {
                 serviceBinder.startServiceThreads();
 
-                // Modify...
-                new LocalIPAddressTask() {
-                    @Override
-                    public void onPostExecute(InetAddress inetAddress) {
-                        int port = Integer.parseInt(sharedPrefs.getString("nodePort", "3000"));
-                        URL url;
-
-                        try {
-                            url = new URL("http", inetAddress.getHostAddress(), port, "");
-                            findPreference("browseNow").setSummary("Browse to " + url.toString());
-                            findPreference("browseNow").setEnabled(true);
-
-                        } catch (MalformedURLException e) {
-                            Log.e(TAG, "Cannot form URL", e);
-                        }
-                    }
-                }.execute();
 
                 return true;
             }
@@ -97,31 +80,10 @@ public class ProcessExplorerSettingsActivity extends PreferenceActivity
         });
     }
 
-    protected void startService() throws Exception {
-        Intent servIntent;
-
-        servIntent = new Intent(this, ProcessExplorerService.class);
-        servConn = new ProcessExplorerServiceConnection(this);
-
-        /*
-         * FIXME: I'm absolutely not sure we can call bindService immediately after bindService but
-         * I haven't found anything in the documentation that says otherwise.
-         */
-        if (startService(servIntent) != null)
-            bindService(servIntent, servConn, BIND_AUTO_CREATE);
-        else
-            throw new Exception("Failed to start service");
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void checkExtractTask() {
         final ProgressDialog progDialog;
         AssetExtractTaskParams extractTaskParams;
         AssetExtractTask extractTask;
-
-        super.onCreate(savedInstanceState);
-
-        prepareLayout();
 
         extractTaskParams = new AssetExtractTaskParams();
         extractTaskParams.assetPath = "system-explorer.zip";
@@ -154,6 +116,31 @@ public class ProcessExplorerSettingsActivity extends PreferenceActivity
         }
         else
             Log.i(TAG, "Not extracting assets.");
+
+    }
+
+    protected void startService() throws Exception {
+        Intent servIntent;
+
+        servIntent = new Intent(this, ProcessExplorerService.class);
+        servConn = new ProcessExplorerServiceConnection(this);
+
+        /*
+         * FIXME: I'm absolutely not sure we can call bindService immediately after bindService but
+         * I haven't found anything in the documentation that says otherwise.
+         */
+        if (startService(servIntent) != null)
+            bindService(servIntent, servConn, BIND_AUTO_CREATE);
+        else
+            throw new Exception("Failed to start service");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        prepareLayout();
+        checkExtractTask();
     }
 
     @Override
@@ -183,6 +170,9 @@ public class ProcessExplorerSettingsActivity extends PreferenceActivity
 
         serviceBinder = service;
         serviceBinder.addNodeThreadListener(this);
+
+        if (serviceBinder.isNodeProcessRunning())
+            updateOnServiceStarted();
     }
 
     @Override
@@ -192,31 +182,58 @@ public class ProcessExplorerSettingsActivity extends PreferenceActivity
         serviceBinder = null;
     }
 
+    /**
+     * All the actions that are to be executed when the service stops or is stopped
+     * when the activity is started.
+     */
+    protected void updateOnServiceStopped() {
+        findPreference("stopNow").setEnabled(false);
+        findPreference("startNow").setEnabled(true);
+    }
+
+    /**
+     * All the actions that are to be executed when the service starts or is started
+     * when the activity is started.
+     */
+    protected void updateOnServiceStarted() {
+        final SharedPreferences sharedPrefs;
+
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Enable the Browse Now button..
+        new LocalIPAddressTask() {
+            @Override
+            public void onPostExecute(InetAddress inetAddress) {
+                int port = Integer.parseInt(sharedPrefs.getString("nodePort", "3000"));
+                URL url;
+
+                try {
+                    url = new URL("http", inetAddress.getHostAddress(), port, "");
+                    findPreference("browseNow").setSummary("Browse to " + url.toString());
+                    findPreference("browseNow").setEnabled(true);
+
+                } catch (MalformedURLException e) {
+                    Log.e(TAG, "Cannot form URL", e);
+                }
+            }
+        }.execute();
+
+        findPreference("startNow").setEnabled(false);
+        findPreference("stopNow").setEnabled(true);
+    }
+
     @Override
     public void ProcessExplorerServiceEvent(NodeThreadEvent ev, NodeThreadEventData evData) {
         switch (ev) {
             case NODE_STARTED:
                 Log.d(TAG, "Received NODE_STARTED");
-                break;
-
-            case NODE_QUIT:
-                Log.d(TAG, "Received NODE_QUIT");
+                updateOnServiceStarted();
                 break;
 
             case NODE_ERROR:
-                Log.d(TAG, "Received NODE_ERROR");
-                break;
-
-            case NODE_STARTING:
-                Log.d(TAG, "Received NODE_STARTING");
-                break;
-
-            case NODE_STOPPING:
-                Log.d(TAG, "Received NODE_STOPPING");
-                break;
-
             case NODE_STOPPED:
                 Log.d(TAG, "Received NODE_STOPPED");
+                updateOnServiceStopped();
                 break;
         }
     }
